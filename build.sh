420cc99ca8b39c4880f7495dd0f0dc9ae443882c
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2021-04-16 18:56:36 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#############
+# VARIABLES #
+#############
+image_tag__dflt=latest
+base_tag__dflt=latest
+
+#############
+# FUNCTIONS #
+#############
+# $1: variable by reference; $2: default value
+_read() {
+  local -n varname=$1
+  local default=$2
+
+  local s_default="<null>"
+  [ -n "${default}" ] && s_default="${default}"
+  echo "Input '$1' value [${s_default}]:"
+
+  if [ -n "${varname}" ]
+  then
+    echo "${varname}"
+  else
+    read -r varname
+    [ -z "${varname}" ] && varname=${default}
+  fi
+}
+
+build_project_image() {
+  echo
+  echo "=== Build h1mock image ==="
+  echo
+  _read image_tag "${image_tag__dflt}"
+  _read base_tag "${base_tag__dflt}"
+
+  bargs="--build-arg base_tag=${base_tag}"
+
+  set -x
+  # shellcheck disable=SC2086
+  docker build --rm ${bargs} -t testillano/h1mock:"${image_tag}" . || return 1
+  set +x
+}
+
+#############
+# EXECUTION #
+#############
+# shellcheck disable=SC2164
+cd "$(dirname "$0")"
+
+build_project_image
+
+exit $?
+
